783c337b440a741fa80308e7f98981752493a15c
pandabeat <chris.chan.is@gmail.com>
2014-09-30 22:39:54 +0000

    standard tasks all complete

--- rotten_mangoes/app/controllers/reviews_controller.rb
+++ rotten_mangoes/app/controllers/reviews_controller.rb
@@ -1,12 +1,19 @@
 class ReviewsController < ApplicationController
- def new
-    @movie = Movie.find(params[:movie_id])
+ 
+  before_filter :load_movie, :restrict_access
+  # => Load the parent resource so we don't have to 
+  #    keep typing '@movie = Movie.find(params[:movie_id])' 
+
+  def new
+    # @movie = Movie.find(params[:movie_id])
+    # => don't need the above line any more becaue of before_filter (protect method)
     @review = @movie.reviews.build
     # => same as @review = Review.new(movie_id: @movie.id)
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
+    # @movie = Movie.find(params[:movie_id]) 
+    # => don't need the above line any more becaue of before_filter (protect method)
     @review = @movie.reviews.build(review_params)
     @review.user_id = current_user.id
 
@@ -18,6 +25,10 @@ class ReviewsController < ApplicationController
   end
 
   protected
+
+  def load_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 
   def review_params
     params.require(:review).permit(:text, :rating_out_of_ten)
